e9f1008b7be4a862e2c0f53eb94554c3fca5007e
Jospeh Kalb <kalb.joseph@gmail.com>
2016-09-26 13:50:35 -0700

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-// monthly interest rate       
-//monthlyInterestRate = (interestRate / 100) / period          
+/* Creating the drop down list and making a calculations
+for the function(I don't like having 9 variables that 
+I only use once...)*/
 
-// number of payments
-//numberOfPayments = loanTerm * period
-
-// compounded interest rate
-//compoundedIntestRate = Math.pow((1 + monthlyInterestRate), numberOfPayments)  
-
-// interest quotient
-//interestQuotient  = (monthlyInterestRate * compoundedInterestRate) / (compoundedIntestRate - 1)
-
-// final calculation
-//monthlyPayment = loanBalance * interestQuotient;
-
+// makig the drop down options
 $("select").append( $('<option/>').val(12).html("Monthly"));
 $("select").append( $('<option/>').val(6).html("Bimonthly"));
 
-// creating the button
+// creating the button *click* and doing the calculation
 $("button").click(function(){
 	var loanBalance = $("#loan").val();
 	var interestRate = $("#rate").val();
